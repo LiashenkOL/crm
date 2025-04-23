1c8f7fe009911d4e958a43bbe329ef67376df22e
Oleksandr Liashenko <o.liashenko.rs01@kpi.ua>
2025-04-23 16:42:37 +0300

    First Controller and Route

--- app/Http/Controllers/Api/V1/TasksController.php
+++ app/Http/Controllers/Api/V1/TasksController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Models\Tasks;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        //
+        return Tasks::all();
     }
 
     /**
